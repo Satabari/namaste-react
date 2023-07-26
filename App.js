087bd39a4ddf591c9f2fd8e0a86bcc9dcd4e3717
Satabari <satabari.mukherjee@gmail.com>
2023-07-26 23:26:03 +0530

    jsx intro

--- App.js
+++ App.js
@@ -1,53 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div", {
-    id: "parent1"
-  }, 
-  React.createElement(
-    "div", {
-      id: "child1"
-    },            
-    [
-      [
-        React.createElement(
-          "h1", {
-            id: "grandchild11"
-          },
-          "Hello from grandchild11 test"
-        ),
-        React.createElement(
-          "h2", {
-            id: "grandchild12"
-          },
-          "Hello from grandchild12"
-        )
-      ],
-      [
-        React.createElement(
-          "h1", {
-            id: "grandchild21"
-          },
-          "Hello from grandchild21"
-        ),
-        React.createElement(
-          "h2", {
-            id: "grandchild22"
-          },
-          "Hello from grandchild22"
-        )
-      ]
-    ]
-  )
-);
-
-const heading = React.createElement(
-  "h1", {
-    id: "heading"
-  }, 
-  "Hello world from react"
-);
+const jsxHeading = <h1 id="heading">Introduction to jsx</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent)
+root.render(jsxHeading);
+// root.render(parent)
