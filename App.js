aa60609db90a561faf42b0ba8a001519382e4bbd
Satabari <satabari.mukherjee@gmail.com>
2023-09-24 20:38:57 +0530

    creating a react element

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id="heading">Introduction to jsx</h1>
+// const jsxHeading = <h1 id="heading">Introduction to jsx</h1>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+// component composition
+// component within a component
+
+// React Fragment - behaves like an empty tag
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      <h1 className="heaidng">Namaste React Fucntional Component</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
-// root.render(parent)
+
+root.render(<HeadingComponent />);
